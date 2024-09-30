38a950e255662f233d4ddffdef305ebc91664d96
leukelele <70491331+leukelele@users.noreply.github.com>
2024-09-30 16:40:43 -0500

    small syntax change for consistency

--- lc-c-two-sum/main.c
+++ lc-c-two-sum/main.c
@@ -11,7 +11,7 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int* twoSum(int *nums, int numsSize, int target, int *returnSize) {
-  int* sums = malloc(2*sizeof(int));
+  int* sums = malloc(2 * sizeof(int));
   *returnSize = 0;
 
   for (int i = 0; i < numsSize; i++) {
