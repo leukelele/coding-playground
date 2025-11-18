1b585d02919c87041c6ff759617936b5387d35e0
leukelele <70491331+leukelele@users.noreply.github.com>
2025-11-17 18:25:04 -0600

    true quine

--- code/ft-c-quine/quine.c
+++ code/ft-c-quine/quine.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-char *s = "#include <stdio.h>%c%cchar *s = %c%s%c;%c%cint main(void) {%c    printf(s, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10);%c    return 0;%c}%c%c";
+char *s = "#include <stdio.h>%c%cchar *s = %c%s%c;%c%cint main(void) {%c    printf(s, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10);%c    return 0;%c}%c";
 
 int main(void) { 
     printf(s, 10, 10, 34, s, 34, 10, 10, 10, 10, 10, 10);
     return 0;
 }
+
